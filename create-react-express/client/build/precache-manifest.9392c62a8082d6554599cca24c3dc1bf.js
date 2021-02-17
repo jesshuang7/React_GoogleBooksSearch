206587a30bfd2a2ac2048be8f6df406b7165598a
Jessica Huang <tzunuhuang@gmail.com>
2021-02-17 15:09:18 +0800

    deploying

---
+++ create-react-express/client/build/precache-manifest.9392c62a8082d6554599cca24c3dc1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea5e1d2b08d1b546a230e01578a23d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8634ae9bbc2e99cc9ec",
+    "url": "/static/css/main.68592de9.chunk.css"
+  },
+  {
+    "revision": "c3b810183fd74bc5c2f2",
+    "url": "/static/js/2.941449b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.941449b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8634ae9bbc2e99cc9ec",
+    "url": "/static/js/main.ea8963f5.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
